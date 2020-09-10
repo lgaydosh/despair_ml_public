eff386ca52bc7f4a05935ad5ec11d803c054792e
Charreau Bell <charreau.s.bell@vanderbilt.edu>
2020-09-10 13:12:08 -0500

    modified 0* series to look for kfolds and results directory to close #228 and close #229

--- 01-slurm-experiments.R
+++ 01-slurm-experiments.R
@@ -10,8 +10,11 @@
 args <- commandArgs(trailingOnly = TRUE)
 
 #expected command line parameters
-n_boot = as.numeric(args[[1]])
-task_ID = as.numeric(args[[2]])
+outcome <- as.character(args[[1]])
+notebook_file <- as.character(args[[2]])
+n_boot <- as.numeric(args[[3]])
+task_ID <- as.numeric(args[[4]])
+results_dir <- as.character(args[[5]])
 
 #compatibility with SLURM
 .libPaths("~/R/x86_64-pc-linux-gnu-library/3.6")
@@ -22,10 +25,9 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load(knitr, stringr, readr, fs)
 
 #other parameters
-notebook_file = '81-compute-suicidal'
-outcome = 'h5mn8'
-results_directory = str_c(outcome, '/results_run_2')
-kfold_file = str_c(outcome, '/kfold_assign_3895_tts_9384.csv')
+fbase <- '/scratch/p_gaydosh_lab/DSI/'
+results_directory = str_c(fbase, results_dir)
+kfold_file = str_c(fbase, outcome, '/kfold_assign_3895_tts_9384.csv')
 
 # Convert the 70 series notebook to a script
 purl(paste0(notebook_file, '.Rmd'))
---
+++ 02-project-setup.R
@@ -0,0 +1,83 @@
+######
+##
+## 02-project-setup.R
+##
+## Description: this function builds the *0 source files into a temporary directory.  It also
+## Runs k-fold cross-validation.
+##
+########
+
+## ----command line parsing ----------------------------------
+#get command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+#get variable arguments
+outcome <- as.character(args[[1]])
+legit_skip <- as.logical(args[[2]])
+
+if(as.character(args[[3]])=="NULL"){
+  skip_var <- NULL
+} else {
+  skip_var <- as.numeric(args[[3]])
+}
+
+results_directory <- as.character(args[[4]])
+
+print(outcome)
+print(legit_skip)
+print(skip_var)
+print(results_directory)
+
+## ----load packages----------s------------------------
+pacman::p_load(tidyverse, fs, knitr)
+
+## ----source files-----------------------------------
+source_dir <- "r_project_source"
+
+dir_create(source_dir)
+
+# get only .Rmd files and source them
+list.files(pattern = "[0-8]0-\\w{1,}-?\\w{1,}.Rmd$") %>% 
+  map(purl)
+
+# move source files to temporary directory
+list.files(pattern = "[0-8]0-\\w{1,}-?\\w{1,}.[R]$") %>% 
+  map(file_move, new_path = str_c(source_dir, '/'))
+
+## ---- check for kfold -----------------------------------
+fbase <- '/scratch/p_gaydosh_lab/DSI/'
+outcome_dir <- str_c(fbase, outcome)
+print('Checking your kfold file...')
+
+## check to see if kfold files exist, and if they don't, create them
+if(!dir_exists(outcome_dir)){
+  print(str_c("the directory ", outcome_dir, " does not appear to exist.  Creating..."))
+  
+  #create the outcome directory
+  dir_create(outcome_dir)
+}
+
+#check to see if there's a kfold file in the directory
+pat <- 'kfold'
+dir_contents <- list.files(outcome_dir)
+in_files <- map(dir_contents, function(x) grep(pat, x))
+res <- unlist(in_files)
+
+#if there isn't a kfold file, create them with the args above
+if(sum(res)==0)
+  print("Looks like you haven't made your kfold splits yet.  Making them now...")
+  source("86-slurm-kfold-helper.R")
+
+## ---- Check for results directory -----------------------------------
+print('Checking your results directory...')
+results_dir <- str_c(fbase, results_directory)
+
+if(!dir_exists(results_dir)){
+  print(str_c("Looks like your desired outcome directory: ", results_dir, " doesn't exist.  Creating..."))
+  dir_create(results_dir, recurse=TRUE)
+}
+
+print("Setup file has finished.")
+
+
+
